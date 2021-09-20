2c5dd634880ddb241aa5ee477ac7b34941be0c60
Adan Gomez <adanvgomez1234@gmail.com>
2021-09-20 10:26:38 -0300

    Se agrego funcion para cargar nuevo resultado y se lo llama en el main

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"tpGoTudai2021/model"
+)
+
 func main() {
 
+	r := model.NewResult("NN", 03, "001")
+	fmt.Println(r)
 }
--- model/structure.go
+++ model/structure.go
@@ -1,11 +1,25 @@
 package model
 
+import "strconv"
+
 type Result struct {
 	Type   string
-	Value  string
 	Length int
+	Value  string
 }
 
-func NewResult(types string, value string, length int) Result {
-	return Result{types, value, length}
+func NewResult(Type string, Length int, Value string) Result {
+	return Result{Type, Length, Value}
+}
+
+func changeType(values string) Result {
+	t := values[0:2]
+	l := values[2:4]
+	v := values[4:]
+	lInt, err := strconv.Atoi(l) //convierte string en int
+	if err == nil {
+		return NewResult(t, lInt, v)
+	} else {
+		return NewResult("Error", 00, "Error")
+	}
 }
